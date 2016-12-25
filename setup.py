3bb65c08488a32469eec8a011d4fa1232ec84de8
JeongHyeon Yun <wyun13043@gmail.com>
2016-12-25 18:31:54 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='shopify-multipass',
     version='1.0.0',
     description='Shopify Multipass module for Python',
-    url='https://github.com/wyun13043/shopify-multipass',
+    url='https://github.com/gureuso/shopify-multipass',
     author='gureuso',
     author_email='wyun13043@gmail.com',
     license='Apache 2.0 License',
